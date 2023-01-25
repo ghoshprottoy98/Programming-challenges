721f185edd610d4bd6e824f5a11e9c0c79aabfa0
PROTTOY GHOSH <107128182+ghoshprottoy98@users.noreply.github.com>
2023-01-25 21:12:19 +0600

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -20,3 +20,27 @@ int main()
     cout << " The Greatest Common Divisor is: " << gcd << endl;
     return 0;
 }
+
+
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+ 
+int main()
+{
+    int n = 9;
+    cout << fib(n);
+    getchar();
+    return 0;
+}
+
+
+
+
+
+
+
