a526aff057b1df787f520cbcaf16d1d948b20cae
PROTTOY GHOSH <107128182+ghoshprottoy98@users.noreply.github.com>
2023-01-27 20:53:41 +0600

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -66,5 +66,24 @@ int main()
 
 
 
+int MissingNo (int a[], int n)
+{
+  int i, total;
+  total = (n + 1) * (n + 2) / 2;
+  for (i = 0; i < n; i++)
+  total -= a[i];
+  return total;
+}
+
+int main ()
+{
+    int a[] = { 1, 2, 4, 5, 6 };
+ 
+    int n = sizeof(a) / sizeof(int);
+ 
+    cout << MissingNo (a, n) << endl;
+    return 0;
+}
+
 
 
